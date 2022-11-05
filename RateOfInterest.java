3ad2bcfae09b90f8fd1b74d857152f35ea0c64b9
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 13:06:14 +0530

    Update RateOfInterest.java

--- RateOfInterest.java
+++ RateOfInterest.java
@@ -1,6 +1,12 @@
+/*Accept age and gender from user and display the rate of interest as follows 
+age above 60 and gender male(RI= 7%)
+age above 60 and gender female(RI= 7.5%)
+below 60 age = RI 5% */
+
+
 package tanu31;
-import java.util.Scanner;
-public class RateOfInterest 
+import java.util.Scanner;//importing scanner class
+public class RateOfInterest //declaring class
 {
 	public static void main(String[] args) {
 		AgeRI();
@@ -10,14 +16,14 @@ public class RateOfInterest
 	{
 		int age;
 		 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // to accept user input
 		System.out.println("Enter Age: ");
 		age = sc.nextInt();
 		System.out.println("Enter Gender M/F?");
 		char gender= sc.next().charAt(0);
 		if(gender == 'M' || gender == 'm')
 		{
-			if(age>60 )
+			if(age>60 ) //conditional statement 
 			{
 				System.out.println("The Rate of Interest is 7% ");
 			}
