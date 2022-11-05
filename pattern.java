bc6323ead3dec497abad9fd5a455f50d5ab76892
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 12:43:09 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,30 +1,20 @@
 package tanu31;
 public class pattern {
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		char ch = 'A';
 
-	
+		for (int i = 0; i <= 4; i++) {
 
-		public static void main(String[] args) 
-		{
-			// TODO Auto-generated method stub
-			int i,j;
-			for(i=1;i<=5;i++)
-			{
-				for(j=4;j>=i;j--)
-				{
-					System.out.print(" ");	
-				}
-				for(j=1;j<=i;j++)
-				{
-						if(j%2==0)
-						{
-							System.out.print("0");
-						}
-						else
-						{
-							System.out.print("1");
-						}
-				}
-				System.out.println( );
-			}	
+		for (int j = 0; j <= i; j++) {
+
+			System.out.print(ch++);}
+
+		System.out.println();
+
 		}
+
 	}
+
+}
