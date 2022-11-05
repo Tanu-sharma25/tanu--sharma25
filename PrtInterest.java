a6d752088e450d6f5bc39ec093d582faed518f49
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 12:00:57 +0530

    Update PrtInterest.java

--- PrtInterest.java
+++ PrtInterest.java
@@ -1,13 +1,13 @@
 
+//Write a java program to calculate the simple interest//
 
-
-import java.util.Scanner; 
-public class PrtInterest
+import java.util.Scanner; //importing package to use scanner class
+public class PrtInterest // class declaration
 { 
-public static void main(String args[]) 
+public static void main(String args[]) //main method
 { 
-float p, r, t, sinterest; 
-Scanner scan = new Scanner(System.in); 
+float p, r, t, sinterest; //variable declaration
+Scanner scan = new Scanner(System.in); //scanner class to accept user input
 System.out.print("Enter the Principal : "); 
 p = scan.nextFloat();
 System.out.print("Enter the Rate of interest : "); 
@@ -15,7 +15,7 @@ r = scan.nextFloat();
 System.out.print("Enter the Time period : "); 
 t = scan.nextFloat(); 
 scan.close(); 
-sinterest = (p * r * t) / 100; 
+sinterest = (p * r * t) / 100; //formula used to calculate simple interest
 System.out.print("Simple Interest is: " +sinterest); 
 } 
 }
