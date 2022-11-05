55993fa7b8bbe68ab809eea07b2895774fdc4f1a
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 13:33:10 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,15 +1,23 @@
-package tanu31;
+/*write a program to display following pattern
+****
+*  *
+*  *
+****   */
 
-import java.util.Scanner;
+package tanu31; //importing package
 
-public class Pattern3 {
-	public static void main(String[] args) {
+import java.util.Scanner; //importing scanner class
+
+public class Pattern3 //declaring class
+{
+	public static void main(String[] args) //main method
+	{
 		// TODO Auto-generated method stub
-		for(int i=0;i<4;i++)
+		for(int i=0;i<4;i++) //for displaying row
 		{
-		for(int j=0;j<4;j++)
+		for(int j=0;j<4;j++) //for displaying column
 		{
-		if(i==0||j==0||j==3||i==3)
+		if(i==0||j==0||j==3||i==3) //for displaying the mentioned pattern
 		{
 		System.out.print("*");
 		}
