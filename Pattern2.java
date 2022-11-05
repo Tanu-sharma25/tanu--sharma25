b4d533b712ac8196e185fb7426f43064bf522ded
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 13:26:22 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,27 +1,30 @@
-package tanu31;
+/*write a program to display following pattern
+  
+*********
+ *******
+  *****
+   ***
+    *  */
 
-import java.util.Scanner;
+package tanu31; //importing package
 
-public class Pattern2 {
-	public static void main(String[] args) {
+import java.util.Scanner; //importing scanner class 
+
+public class Pattern2 //decalring a class
+{
+	public static void main(String[] args) //main method
+	{
 		// TODO Auto-generated method stub
-		for(int i=0;i<4;i++)
+		for(int i=5;i>=1;i--) //printing no of rows 
 		{
-		for(int j=0;j<4;j++)
-		{
-		if(i==0||j==0||j==3||i==3)
-		{
-		System.out.print("*");
+			for(int j=5;j>i;j--)// printing no of columns
+			{
+				System.out.print(" ");
+			}
+			for (int j=1;j<=(2*i-1);j++) //displaying pattern
+			{
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		else
-		{
-		System.out.print(" ");
-		}
-		}
-		System.out.println();
-		}
-
-	}
-
-
-	}
+	}}
