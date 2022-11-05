70d8f518c6ad73e10bd6d9d7d4203533ba97a854
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 11:46:25 +0530

    Update CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -1,17 +1,17 @@
+//write a java program to find the area and circumference of the circle also take radius as user input//
 
+import java.util.*; //impoting util package
 
-import java.util.*;
-
-public class CircleArea 
+public class CircleArea //declaring a class
 {
-public static void main(String[] args) 
+public static void main(String[] args) // main method
 {
-double radius, area, circumference;
-Scanner in = new Scanner(System.in);
+double radius, area, circumference; //variable declartion
+Scanner in = new Scanner(System.in); //scanner class to accept user input
 System.out.println("Enter Radius of Circle:");
 radius = in.nextDouble();
-area = Math.PI * radius * radius;
-circumference = 2 * Math.PI * radius;
+area = Math.PI * radius * radius; //calculating area of the circle 
+circumference = 2 * Math.PI * radius; //calculating circumference of the circle
 System.out.println("Area of Circle : " + area);
 System.out.println("Circumference of Circle : " + circumference);
 }
