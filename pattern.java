4b8db36e31b9e69a0da0df0a9bb8c58d701ee8c0
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 13:00:25 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,9 +1,16 @@
-package tanu31;
+/*write a program to display following pattern
+A
+BC
+DEF
+GHIJ
+KLMNO */
+
+package tanu31; // importing package
 public class pattern {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		char ch = 'A';
+		char ch = 'A'; //declaring character variable
 
 		for (int i = 0; i <= 4; i++) {
 
