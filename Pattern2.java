5cab5c548dda8e651e88429600aa1c8d373236a5
Tanu-sharma25 <116855580+Tanu-sharma25@users.noreply.github.com>
2022-11-05 13:22:39 +0530

    Add files via upload

--- Pattern2.java
+++ Pattern2.java
@@ -1,30 +1,27 @@
-/*write a program to display following pattern
-  
-*********
- *******
-  *****
-   ***
-    *  */
+package tanu31;
 
-package tanu31; //importing package
+import java.util.Scanner;
 
-import java.util.Scanner; //importing scanner class 
-
-public class Pattern2 //decalring a class
-{
-	public static void main(String[] args) //main method
-	{
+public class Pattern2 {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=5;i>=1;i--) //printing no of rows 
+		for(int i=0;i<4;i++)
 		{
-			for(int j=5;j>i;j--)// printing no of columns
-			{
-				System.out.print(" ");
-			}
-			for (int j=1;j<=(2*i-1);j++) //displaying pattern
-			{
-				System.out.print("*");
-			}
-			System.out.println();
+		for(int j=0;j<4;j++)
+		{
+		if(i==0||j==0||j==3||i==3)
+		{
+		System.out.print("*");
 		}
-	}}
+		else
+		{
+		System.out.print(" ");
+		}
+		}
+		System.out.println();
+		}
+
+	}
+
+
+	}
